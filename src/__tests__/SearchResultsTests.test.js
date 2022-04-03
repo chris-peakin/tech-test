97b6bc3a20662cef646dfb5689156d8293b7b3e7
Chris Peakin <christopher.peakin@googlemail.com>
2022-04-03 15:21:21 +0100

    updated tests

--- src/__tests__/SearchResultsTests.test.js
+++ src/__tests__/SearchResultsTests.test.js
@@ -3,8 +3,8 @@ import {render} from "@testing-library/react";
 import SearchResults from "../components/SearchResultsComponent";
 
 describe("search results component", () => {
-    //Look at this again.
-    const {asFragment} = render(<SearchResults results={SearchResults}/>);
+    const validProps = ["image1", "image2", "image3"]
+    const {asFragment} = render(<SearchResults results={validProps}/>);
 
     it("search results component renders correctly", () => {
         expect(asFragment).toMatchSnapshot();
--- src/__tests__/SearchTests.test.js
+++ src/__tests__/SearchTests.test.js
@@ -3,7 +3,8 @@ import { render } from "@testing-library/react";
 import Search from "../components/SearchComponent";
 
 describe("search component", () =>{
-    const {asFragment} = render(<Search/>);
+    const validProps = "generic search term";
+    const {asFragment} = render(<Search setSearchResults = {validProps} />);
 
     it("search component renders correctly", () =>{
         expect(asFragment).toMatchSnapshot();
